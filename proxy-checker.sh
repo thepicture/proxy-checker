9170f9d473f8adbf044f74a9064d844554d459ea
thepicture <85333076+thepicture@users.noreply.github.com>
2022-12-01 20:19:21 -0500

    feat: align output in the result file

--- proxy-checker.sh
+++ proxy-checker.sh
@@ -2,8 +2,10 @@ for proxy in $(cat $1); do
     response=$(curl -IL --silent -x $proxy $3 | grep -m 1 "HTTP")
     http_code=$(echo "$response" | awk "{print$ 2}")
     date=$(date)
-    if [[ $http_code = "" ]]; then
+    if [[ -z $http_code ]]; then
         http_code=ERR
+        response="CONNECT failed"
     fi
     echo [$date] [$http_code] [$proxy] $response | tee -a $2
 done
+cat $2 | column -t -s "]" -l 4 --output-separator="]" | tee $2 >/dev/null
